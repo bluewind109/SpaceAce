098189050a1ec33061f00363f3280080ec0bd98b
Hoàng Quốc Khánh <khanh.hoangquoc91@gmail.com>
2024-04-30 18:12:13 +0700

    Implement enemy's shooting mechanics

---
+++ utils/utils.gd
@@ -0,0 +1,20 @@
+extends Node
+
+class_name Utils
+
+static func set_and_start_timer(
+	timer: Timer,
+	target: float,
+	variance: float
+) -> void:
+	timer.wait_time = randf_range(
+		target - variance,
+		target + variance
+	)
+	timer.start()
+
+
+
+
+
+
